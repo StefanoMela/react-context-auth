fcd39b964e14fc5bde6e04dcfd6ce543ff9d0464
StefanoMela <stefano.mela25@gmail.com>
2024-06-24 17:28:16 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import DefaultLayout from "./pages/DefaultLayout";
+import DefaultLayout from "./layouts/DefaultLayout";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import PostList from "./pages/PostList";
 import Post from "./pages/Post";
 
-import GlobalContext from "./contexts/GlobalContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 import "./App.css";
 
+
+import DashboardLayout from "./layouts/DashboardLayout";
+
 export default function App() {
   return (
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
             <Route path="*" element={<NotFound />} />
             <Route index element={<Home />} />
+          </Route>
             <Route path="/posts" element={<PostList />} />
             <Route path="/posts/:postId" element={<Post />} />
+          <Route path="/admin"  element={<DashboardLayout />}>
           </Route>
         </Routes>
       </BrowserRouter>
+    </AuthProvider>
   );
 }
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,3 +1,40 @@
 import { createContext } from "react";
+import { useState, useContext } from "react";
 
-export const AuthContext = createContext({})
+const AuthContext = createContext({})
+
+const AuthProvider = ({ children }) => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const handleLogin = (payload, ) => {
+        setIsLoggedIn(true);
+    }
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+    }
+
+    const value = {
+        isLoggedIn,
+        handleLogin,
+        handleLogout
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+const useAuth = () => {
+    const value = useContext(AuthContext);
+    if(value === undefined) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+
+    return value;
+}
+
+export { AuthProvider, useAuth };
